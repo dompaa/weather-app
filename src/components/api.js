d4aef041cf06a24db36a30e32f5d14628cd0768c
Dominik <gradiskid@gmail.com>
2022-09-02 17:43:16 +0200

    build city search component

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7e245af4d4msh105e03b57e4ac7ep199318jsn3108acca9b3a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+ 
