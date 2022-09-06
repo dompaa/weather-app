8224750a1345906feeb2385b49cb5300418d28d5
Dominik <gradiskid@gmail.com>
2022-09-06 23:59:57 +0200

    added component forecast

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,14 @@
+import { Accordion } from "react-accessible-accordion"
+
+const Forecast = (data) => {
+    return (
+    <>
+        <label className="title">Daily</label>
+        <Accordion allowZeroExpanded>
+
+        </Accordion>
+    </>
+    )
+}
+
+export default Forecast
