ebd2407859ad21c4a993563e3a637b9e43ee7029
Dominik <gradiskid@gmail.com>
2022-09-09 00:17:30 +0200

    expressed label, description and temp per day

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -7,19 +7,23 @@ const Forecast = ({ data }) => {
     const forecastDays = WEEK_DAYS.slice(dayINAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0,dayINAWeek)
     );
 
-    console.log(forecastDays);
+    // for testing the days in in the week
+    // console.log(forecastDays);
 
     return (
     <>
         <label className="title">Daily</label>
         <Accordion allowZeroExpanded>
-            {data.list.slice(0, 7).map((item,idx) => (
-                <AccordionItem key={idx}>
+            {data.list.slice(0, 7).map((item,index) => (
+                <AccordionItem key={index}>
                     <AccordionItemHeading>
                         <AccordionItemButton>
                             <div className="daily-item">
                                 <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
-                                <label className="day"></label>
+                                <label className="day">{forecastDays[index]}</label>
+                                <label className="description">{item.weather[0].description}</label>
+                                <label className="min-max">{Math.round(item.main.temp_min)}°C / {Math.round(item.main.temp_max)}°C</label>
+                                
                             </div>
                         </AccordionItemButton>
                     </AccordionItemHeading>
